7f9f778218cf44b30a98e34b35fdbeaa7c31b26d
Seohyeon <0205lucy@naver.com>
2022-06-26 19:07:13 +0900

    navbar

--- src/home/Navbar.js
+++ src/home/Navbar.js
@@ -1,14 +1,38 @@
 import styles from './css/Navbar.module.css';
 import {Link} from 'react-router-dom';
 function Navbar(){
+    const path='./img/';
     return(
         <div className={styles.nav}>
-        <Link to="/" className={styles.link}><div className={styles.home}>홈</div></Link>
-        <Link to="/" className={styles.link}><div className={styles.location}>내주변</div></Link>
-        <Link to="/search" className={styles.link}><div className={styles.search}>검색</div></Link>
-        <Link to="/heart" className={styles.link}><div className={styles.heart}>찜</div></Link>
-        <Link to= "/mypage" className={styles.link}><div className={styles.mypage}>마이페이지</div></Link>
+            <div className={styles.navitem}>
+                <Link to="/" className={styles.link}>
+                    <img src={require(`${path}home.png`)} className={styles.img} alt="홈"></img>
+                    <div className={styles.home}>홈</div>
+                </Link>
+
+                <Link to="/" className={styles.link}>
+                    <img src={require(`${path}near.png`)} className={styles.img} alt="홈"></img>
+                    <div className={styles.location}>내주변</div>
+                </Link>
+
+                <Link to="/search" className={styles.link}>
+                    <img src={require(`${path}search.png`)} className={styles.img} alt="홈"></img>
+                    <div className={styles.search}>검색</div>
+                </Link>
+
+                <Link to="/heart" className={styles.link}>
+                    <img src={require(`${path}heart.png`)} className={styles.img} alt="홈"></img>
+                    <div className={styles.heart}>찜</div>
+                </Link>
+
+                <Link to= "/mypage" className={styles.link}>
+                    <img src={require(`${path}mypage.png`)} className={styles.img} alt="홈"></img>
+                    <div className={styles.mypage}>마이페이지</div>
+                </Link>
+            </div>
+
         </div>
+
 );
 }
 export default Navbar;
