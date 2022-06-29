900702ce10a47ca8bca20ad85493bcb9deccd27e
Seohyeon <0205lucy@naver.com>
2022-06-29 14:50:01 +0900

    mypagecontainer

--- src/mypage/Mypage.js
+++ src/mypage/Mypage.js
@@ -5,13 +5,13 @@ import {Link} from 'react-router-dom';
 
 function Mypage(){
     return (
-        <div className={styles.container}>
-        {/* 여기 안에만 넣자 */}
+        <div className={styles.background}>
+            <div className={styles.container}>
                 <div className={styles.title}>마이페이지다</div>
                 <Link to= "/login" className={styles.link}><div className={styles.login}>로그인/회원가입</div></Link>
                 <Navbar></Navbar>
                 <Mypagemenu></Mypagemenu>
-        {/* 여기 안에만 넣자 */}
+            </div>
         </div> 
     );
 }
