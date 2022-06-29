8e4e2f507dd31c6481e997448fe00b5b4c5c95ab
Seohyeon <0205lucy@naver.com>
2022-06-30 00:43:40 +0900

    category edit

--- src/home/Category.js
+++ src/home/Category.js
@@ -11,9 +11,9 @@ function Category(){
           <div className={styles.option}><img src={require(`${path}cookie.png`)} className={styles.img} alt="쿠키"/>쿠키</div>
           <div className={styles.option}><img src={require(`${path}scone.png`)} className={styles.img} alt="스콘"/>스콘</div>
           <div className={styles.option}><img src={require(`${path}dango.png`)} className={styles.img} alt="스콘"/>당고</div>
-          <div className={styles.option}><img src={require(`${path}ricecake.png`)} className={styles.img} alt="스콘"/>떡</div>
-          <div className={styles.option}><img src={require(`${path}parfait.png`)} className={styles.img} alt="스콘"/>파르페</div>
-          <div className={styles.option}><img src={require(`${path}tart.png`)} className={styles.img} alt="스콘"/>에그타르트</div>
+          <div className={styles.option}><img src={require(`${path}ricecake.png`)} className={styles.imgrice} alt="스콘"/>떡</div>
+          <div className={styles.option}><img src={require(`${path}parfait.png`)} className={styles.imgpar} alt="스콘"/>파르페</div>
+          <div className={styles.option}><img src={require(`${path}tart.png`)} className={styles.imgegg} alt="스콘"/>에그타르트</div>
           <div className={styles.option}><img src={require(`${path}waffle.png`)} className={styles.img} alt="스콘"/>와플/크로플</div>
         </div>
     );
