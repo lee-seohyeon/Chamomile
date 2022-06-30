8c9f2da4314b19a4de688e94e9b5828a9be160e9
Seohyeon <0205lucy@naver.com>
2022-06-30 23:38:42 +0900

    backbigger

--- src/heart/Heart.js
+++ src/heart/Heart.js
@@ -1,7 +1,8 @@
 import Header from "./Header";
 import Navbar from "../nav/Navbar";
 import styles from "./css/Heart.module.css";
-import Product from "../result/Product";
+// import Product from "../result/Product";
+
 function Heart() {
   return (
     <div className={styles.background}>
