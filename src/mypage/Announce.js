35ed0188bbe3020f9d3cea4f0c54723a061230d2
Seohyeon <0205lucy@naver.com>
2022-07-01 21:55:06 +0900

    onepostfin

--- src/mypage/Announce.js
+++ src/mypage/Announce.js
@@ -12,15 +12,18 @@ function Announce() {
                 <div className={styles.title}>공지사항</div>
                 <div className={styles.fake}></div>
             </div>
-            <hr></hr>
+            {/* <hr></hr> */}
 
             <div className={styles.postcontainer}>
+            <div className={styles.posttextcontainer}>
                 <p className={styles.posttitle}>개인정보처리방침 개정 안내</p>
                 <p className={styles.postdate}>2022.06.23</p>
             </div>
+            <div className={styles.postgo}>  >  </div>
+            </div>
             <hr className={styles.postline}></hr>
 
-            
+
         </div>
         <Navbar></Navbar>
     </div>
