c5b7d30e2655b0ba6e03d47b5ec6bee662d9f4ba
Seohyeon <0205lucy@naver.com>
2022-07-01 21:24:53 +0900

    announce

--- src/mypage/Announce.js
+++ src/mypage/Announce.js
@@ -1,13 +1,17 @@
 import Navbar from "../nav/Navbar";
 import styles from "./css/Announce.module.css";
+import Back from "../back/Back";
 
 
 function Announce() {
   return (
     <div className={styles.background}>
         <div className={styles.container}>
-            공지사항
-            <Navbar></Navbar>
+            <div className={styles.header}>
+                <Back className={styles.fake}></Back>
+                <div className={styles.title}>공지사항</div>
+                <div className={styles.fake}></div>
+            </div>
         </div>
     </div>
   );
