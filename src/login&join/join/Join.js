9ad208212868ba1f1a54277d3f4642dfea70aecf
Seohyeon <0205lucy@naver.com>
2022-07-01 20:53:02 +0900

    push

--- src/login&join/join/Join.js
+++ src/login&join/join/Join.js
@@ -12,7 +12,9 @@ function Join(){
             <div className={styles.text}><p>맛있는 디저트와</p><p>더 가까워져요 :)</p></div>
             </div>
             <hr></hr>
-            <Snslogin></Snslogin>
+            <Snslogin>
+                
+            </Snslogin>
             <hr></hr>
             <div className={styles.startemail}>
             <div className={styles.text2}>이메일로 시작하기</div>
