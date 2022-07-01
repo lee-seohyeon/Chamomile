22d5edc8451294914a97ba2101c505500ad6d88e
Seohyeon <0205lucy@naver.com>
2022-07-01 21:31:54 +0900

    announcepost

--- src/mypage/Announce.js
+++ src/mypage/Announce.js
@@ -12,6 +12,14 @@ function Announce() {
                 <div className={styles.title}>공지사항</div>
                 <div className={styles.fake}></div>
             </div>
+            <hr></hr>
+
+            <div className={styles.postcontainer}>
+                <p className={styles.posttitle}>개인정보처리방침 개정 안내</p>
+                <p className={styles.postdate}>2022.06.23</p>
+                <hr className={styles.postline}></hr>
+            </div>
+
         </div>
         <Navbar></Navbar>
     </div>
