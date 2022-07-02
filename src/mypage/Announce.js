2348eda6a0ad0872057f066c792e7bee793e806a
Seohyeon <0205lucy@naver.com>
2022-07-02 11:30:21 +0900

    :art: 공지사항 이제 다함

--- src/mypage/Announce.js
+++ src/mypage/Announce.js
@@ -26,8 +26,8 @@ function Announce() {
             {/* 이게 포스트 하나임*/}
             <div className={styles.postcontainer}>
             <div className={styles.posttextcontainer}>
-                <p className={styles.posttitle}>개인정보처리방침 개정 안내</p>
-                <p className={styles.postdate}>2022.06.23</p>
+                <p className={styles.posttitle}>서비스 이용약관 개정 안내</p>
+                <p className={styles.postdate}>2022.06.20</p>
             </div>
             <div className={styles.postgo}>  >  </div>
             </div>
@@ -36,8 +36,8 @@ function Announce() {
             {/* 이게 포스트 하나임*/}
             <div className={styles.postcontainer}>
             <div className={styles.posttextcontainer}>
-                <p className={styles.posttitle}>개인정보처리방침 개정 안내</p>
-                <p className={styles.postdate}>2022.06.23</p>
+                <p className={styles.posttitle}>9.6.5 업데이트 안내</p>
+                <p className={styles.postdate}>2022.06.15</p>
             </div>
             <div className={styles.postgo}>  >  </div>
             </div>
@@ -46,8 +46,8 @@ function Announce() {
             {/* 이게 포스트 하나임*/}
             <div className={styles.postcontainer}>
             <div className={styles.posttextcontainer}>
-                <p className={styles.posttitle}>개인정보처리방침 개정 안내</p>
-                <p className={styles.postdate}>2022.06.23</p>
+                <p className={styles.posttitle}>가게 홍보 메뉴얼 안내</p>
+                <p className={styles.postdate}>2022.06.03</p>
             </div>
             <div className={styles.postgo}>  >  </div>
             </div>
@@ -56,8 +56,8 @@ function Announce() {
             {/* 이게 포스트 하나임*/}
             <div className={styles.postcontainer}>
             <div className={styles.posttextcontainer}>
-                <p className={styles.posttitle}>개인정보처리방침 개정 안내</p>
-                <p className={styles.postdate}>2022.06.23</p>
+                <p className={styles.posttitle}>9.6.0 업데이트 안내</p>
+                <p className={styles.postdate}>2022.06.01</p>
             </div>
             <div className={styles.postgo}>  >  </div>
             </div>
@@ -66,8 +66,8 @@ function Announce() {
             {/* 이게 포스트 하나임*/}
             <div className={styles.postcontainer}>
             <div className={styles.posttextcontainer}>
-                <p className={styles.posttitle}>개인정보처리방침 개정 안내</p>
-                <p className={styles.postdate}>2022.06.23</p>
+                <p className={styles.posttitle}>서버 점검 알림 (5.29 AM 01:00~03:00)</p>
+                <p className={styles.postdate}>2022.05.28</p>
             </div>
             <div className={styles.postgo}>  >  </div>
             </div>
@@ -77,8 +77,8 @@ function Announce() {
             {/* 이게 포스트 하나임*/}
             <div className={styles.postcontainer}>
             <div className={styles.posttextcontainer}>
-                <p className={styles.posttitle}>개인정보처리방침 개정 안내</p>
-                <p className={styles.postdate}>2022.06.23</p>
+                <p className={styles.posttitle}>카모마일 4행시 이벤트 당첨자 안내</p>
+                <p className={styles.postdate}>2022.05.20</p>
             </div>
             <div className={styles.postgo}>  >  </div>
             </div>
@@ -87,8 +87,8 @@ function Announce() {
             {/* 이게 포스트 하나임*/}
             <div className={styles.postcontainer}>
             <div className={styles.posttextcontainer}>
-                <p className={styles.posttitle}>개인정보처리방침 개정 안내</p>
-                <p className={styles.postdate}>2022.06.23</p>
+                <p className={styles.posttitle}>운영정책 개정 안내</p>
+                <p className={styles.postdate}>2022.05.02</p>
             </div>
             <div className={styles.postgo}>  >  </div>
             </div>
@@ -97,8 +97,8 @@ function Announce() {
             {/* 이게 포스트 하나임*/}
             <div className={styles.postcontainer}>
             <div className={styles.posttextcontainer}>
-                <p className={styles.posttitle}>개인정보처리방침 개정 안내</p>
-                <p className={styles.postdate}>2022.06.23</p>
+                <p className={styles.posttitle}>고객센터 전화상담 일시 중단 안내</p>
+                <p className={styles.postdate}>2022.04.23</p>
             </div>
             <div className={styles.postgo}>  >  </div>
             </div>
@@ -107,8 +107,8 @@ function Announce() {
             {/* 이게 포스트 하나임*/}
             <div className={styles.postcontainer}>
             <div className={styles.posttextcontainer}>
-                <p className={styles.posttitle}>개인정보처리방침 개정 안내</p>
-                <p className={styles.postdate}>2022.06.23</p>
+                <p className={styles.posttitle}>9.5.3 업데이트 안내</p>
+                <p className={styles.postdate}>2022.03.14</p>
             </div>
             <div className={styles.postgo}>  >  </div>
             </div>
