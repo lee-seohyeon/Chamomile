75b5473bf6e34e64bba76a68df8fbcdfda092026
Seohyeon <0205lucy@naver.com>
2022-07-03 15:53:30 +0900

    :sparkles: mbti test 만들기 시작

--- src/mbtitest/Mbtitest.js
+++ src/mbtitest/Mbtitest.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import styles from './css/mbtitest.module.css';
-// import { Link } from "react-router-dom";
+import Navbar from "../nav/Navbar";
+import Back from '../back/Back';
+import Realtest from './Realtest';
 
 function Mbtitest() {
   return (
-    <div>
-        <div className={styles.plz}>왜 안돼?</div>
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <Back className={styles.fake}></Back>
+          <div className={styles.title}> MBTI 디저트! </div>
+          <div className={styles.fake}></div>
+        </div>
+        <hr></hr>
+        <Realtest></Realtest>
+        <Navbar></Navbar>
+      </div>
     </div>
   )
 }
---
+++ src/mbtitest/Realtest.js
@@ -0,0 +1,12 @@
+import styles from './css/Realtest.module.css'
+
+function Realtest(){
+    return(
+        <div>
+        연결하슈
+        </div>
+);
+
+}
+
+export default Realtest;
