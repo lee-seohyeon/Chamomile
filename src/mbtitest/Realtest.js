8cd82df0238e1a52d28c754e55b231596e58efd9
Seohyeon <0205lucy@naver.com>
2022-07-03 16:13:19 +0900

    :art: mbti test하는중

--- src/mbtitest/Realtest.js
+++ src/mbtitest/Realtest.js
@@ -1,10 +1,23 @@
 import styles from './css/Realtest.module.css'
 
+
+
+// function start() {
+//     $(".start").hide();
+// }
+
+
+
 function Realtest(){
     return(
-        <div>
-        연결하슈
-        </div>
+        <>
+            <div className={styles.start}>
+                <h3> 고고? </h3>
+                <button onclick='start();'>테스트 시작하기</button>
+            </div>
+
+
+        </>
 );
 
 }
