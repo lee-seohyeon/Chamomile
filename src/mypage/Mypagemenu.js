b400137e2f19a531c39824bb64a60125b7edecc3
d0422 <rlfehd2013@naver.com>
2022-07-03 16:28:27 +0900

    :sparkles: 공지사항 및 고객센터
    공지사항 및 고객센터

--- src/mypage/Mypagemenu.js
+++ src/mypage/Mypagemenu.js
@@ -1,34 +1,50 @@
-import styles from './css/Mypagemenu.module.css';
+import styles from "./css/Mypagemenu.module.css";
 import { Link } from "react-router-dom";
 
-function Mypagemenu(){
-    const path='./img/';
-    return(
-        <div className={styles.container}>
-            <div className={styles.menu}>
-                    <img src={require(`${path}infoedit.png`)} className={styles.img} alt="홈"></img>
-                    <div className={styles.content}>회원정보 수정</div>
-            </div>
+function Mypagemenu() {
+  const path = "./img/";
+  return (
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <img
+          src={require(`${path}infoedit.png`)}
+          className={styles.img}
+          alt="홈"
+        ></img>
+        <div className={styles.content}>회원정보 수정</div>
+      </div>
 
-            <Link to="/Announce">
-            <div className={styles.menu}>
-                    <img src={require(`${path}announce.png`)} className={styles.img} alt="홈"></img>
-                    <div className={styles.content}>공지사항</div>
-            </div>
-            </Link>
-
-            <div className={styles.menu}>
-                    <img src={require(`${path}call.png`)} className={styles.img} alt="홈"></img>
-                    <div className={styles.content}>고객센터</div>
-            </div>
-
-            <div className={styles.menu}>
-                    <img src={require(`${path}review.png`)} className={styles.img} alt="홈"></img>
-                    <div className={styles.content}>작성한 리뷰</div>
-            </div>
+      <Link to="/Announce">
+        <div className={styles.menu}>
+          <img
+            src={require(`${path}announce.png`)}
+            className={styles.img}
+            alt="홈"
+          ></img>
+          <div className={styles.content}>공지사항</div>
         </div>
-    )
+      </Link>
+      <Link to="/Service">
+        <div className={styles.menu}>
+          <img
+            src={require(`${path}call.png`)}
+            className={styles.img}
+            alt="홈"
+          ></img>
+          <div className={styles.content}>고객센터</div>
+        </div>
+      </Link>
 
+      <div className={styles.menu}>
+        <img
+          src={require(`${path}review.png`)}
+          className={styles.img}
+          alt="홈"
+        ></img>
+        <div className={styles.content}>작성한 리뷰</div>
+      </div>
+    </div>
+  );
 }
 
 export default Mypagemenu;
