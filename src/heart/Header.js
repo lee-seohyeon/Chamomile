5eaabf27af43f2503cf8264cd4de515bff37a169
Seohyeon <0205lucy@naver.com>
2022-07-04 23:21:18 +0900

    :lipstick: 찜한 메뉴 header ui 수정

--- src/heart/Header.js
+++ src/heart/Header.js
@@ -1,26 +1,28 @@
 import styles from "./css/header.module.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Back from "../back/Back";
 function Header() {
+  const path = "./img/";
   return (
     <div className={styles.container}>
       <header>
-        <Back className={styles.back}></Back>
-        <div className={styles.title}>찜한목록</div>
-        <div className={styles.fake}></div>
+          <Back className={styles.fake}></Back>
+          <div className={styles.title}>찜한 메뉴</div>
+          <img src={require(`${path}menu.png`)} className={styles.menuimg} alt="noimg"></img>
       </header>
-      <section>
-        <div className={styles.information}>찜한 상품 n개</div>
+
+      <img src={require(`${path}category.png`)} className={styles.categoryimg} alt="noimg"></img>
+      <img src={require(`${path}folder.png`)} className={styles.folderimg} alt="noimg"></img>
+
+      {/* <section>
         <select>
           <option>리뷰많은순</option>
           <option>거리순</option>
           <option>낮은가격순</option>
           <option>높은가격순</option>
         </select>
-      </section>
-      <hr></hr>
-      여기에~~~ 찜한 상품 목록이 드도록 합시다 여기에~~~ 찜한 상품 목록이 드도록
-      합시다여기에~~~ 찜한 상품 목록이 드도록 합시다
+      </section> */}
+
     </div>
   );
 }
