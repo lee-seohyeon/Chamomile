0964a172a5b95ef5ac03ca4634410f9cd47e042f
d0422 <rlfehd2013@naver.com>
2022-07-04 18:17:53 +0900

    :lipstick: 디자인 통일
    디자인 통일

--- src/login&join/join/Input.js
+++ src/login&join/join/Input.js
@@ -1,22 +1,15 @@
-import styles from "./css/Input.module.css"
-function Input({data}){
-    let place="";
-    if (data==="nickname"){
-        place="닉네임";
-    }
-    else if(data==="email"){
-        place="이메일";
-    }
-    else if(data==="password"){
-        place="비밀번호(영문+숫자 8~15자)";
-    }
-    else if(data==="check"){
-        place="비밀번호 확인";
-    }
-    return(
-            <input
-            placeholder={place}
-            className={styles.inputdata}></input>
-    );
+import styles from "./css/Input.module.css";
+function Input({ data }) {
+  let place = "";
+  if (data === "nickname") {
+    place = "닉네임";
+  } else if (data === "email") {
+    place = "이메일";
+  } else if (data === "password") {
+    place = "비밀번호(영문+숫자 8~15자)";
+  } else if (data === "check") {
+    place = "비밀번호 확인";
+  }
+  return <input placeholder={place} className={styles.inputdata}></input>;
 }
 export default Input;
--- src/login&join/join/Join.js
+++ src/login&join/join/Join.js
@@ -3,40 +3,45 @@ import styles from "./css/Join.module.css";
 import Input from "./Input";
 import Snslogin from "../Snslogin";
 import Navbar from "../../nav/Navbar";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function Join(){
-    return(
-        <div className={styles.b}>
-            <div className={styles.container}>
-            <div className={styles.head}>
-            <Back></Back>
-            <div className={styles.text}><p>맛있는 디저트와</p><p>더 가까워져요 :)</p></div>
-            </div>
-            <hr></hr>
-            <Snslogin>
-                
-            </Snslogin>
-            <hr></hr>
-            <div className={styles.startemail}>
-            <div className={styles.text2}>이메일로 시작하기</div>
-            <Input data="nickname"></Input>
-            <Input data="email"></Input>
-            <Input data="password"></Input>
-            <Input data="check"></Input>
-            </div>
-            <Link to="/mypage" className={styles.link}>
-                <div className={styles.submit}>
-                    확인
-                    <Navbar></Navbar>
-                </div>
-            </Link>
-            
-            
+function Join() {
+  return (
+    <div className={styles.b}>
+      <div className={styles.container}>
+        <div className={styles.head}>
+          <Back></Back>
+          <div className={styles.text}>
+            <p>맛있는 디저트와</p>
+            <p>더 가까워져요 :)</p>
+          </div>
         </div>
+        <hr></hr>
+        <Snslogin inner={"join"}></Snslogin>
+        <hr></hr>
+        <div className={styles.startemail}>
+          <div className={styles.text2}>이메일로 시작하기</div>
+          <div className={styles.nick}>
+            <div className={styles.nicknameinput}>
+              <Input data="nickname"></Input>
+            </div>
+            <div className={styles.but}>
+              <button className={styles.check}>중복확인</button>
+            </div>
+          </div>
+          <Input data="email"></Input>
+          <Input data="password"></Input>
+          <Input data="check"></Input>
         </div>
-    )
-
+        <Link to="/mypage" className={styles.link}>
+          <div className={styles.submit}>
+            확인
+            <Navbar></Navbar>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Join;
