340ddcd89b1c4add4b56cf6f5b6c4180884e414d
Seohyeon <0205lucy@naver.com>
2022-07-05 17:55:41 +0900

    :art: 후기 페이지 만들기

--- src/result/Review.js
+++ src/result/Review.js
@@ -1,11 +1,20 @@
-// import styles from "./css/Review.module.css";
-// import { Link } from "react-router-dom";
+import styles from "./css/Review.module.css";
 
 function Review(){
+    const path = "./img/";
     return(
         <>
-            <p>리뷰</p>
-            <p>리뷰</p>
+            <div className={styles.titlebox}>
+                <p className={styles.title}>후기</p>
+                <div className={styles.writebox}>
+                    <img src={require(`${path}pencil.png`)} className={styles.pencilimg} alt="noimg"></img>
+                    <p className={styles.write}>작성하기</p>
+                </div>
+            </div>
+
+
+            <p className={styles.title}>이 가게 다른 메뉴 보기</p>
+
         </>
     );
 }
