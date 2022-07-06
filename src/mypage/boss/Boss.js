f539510c5f2d30e89152dfd2ac2d6ee1f8b6f4c1
Seohyeon <0205lucy@naver.com>
2022-07-06 22:09:34 +0900

    :lipstick: 사장님페이지 ui

--- src/mypage/boss/Boss.js
+++ src/mypage/boss/Boss.js
@@ -17,10 +17,26 @@ function Boss(){
                 </div>
 
                 <div className={styles.subtitle}>영업 관리</div>
-                    <div className={styles.subtext}>가게 이름</div>
-                    <div className={styles.subtext}>전화번호</div>
+
+                    <div className={styles.floor}>
+                        <div className={styles.subtext}>가게 이름</div>
+                        <div className={styles.info}>카모마일</div>
+                    </div>
+
+                    <div className={styles.floor}>
+                        <div className={styles.subtext}>전화번호</div>
+                        <div className={styles.info}>02-123-4567</div>
+                    </div>
+                    
                     <div className={styles.subtext}>주소</div>
+                    <div className={styles.info}>서울시 동작구 흑석동 123-45</div>
+
                     <div className={styles.subtext}>영업일</div>
+                    <div className={styles.info}>
+                        월~금 11:00 ~ 22:00 <br></br>
+                        토 11:00 ~ 22:00 <br></br>
+                        일요일 휴무
+                    </div>
 
 {/* 메뉴 등록 수정 */}
                 <div className={styles.subtitlebox}>
