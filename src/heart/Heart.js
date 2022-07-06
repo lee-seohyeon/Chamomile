407fd82e17250dce4e330d721073beeb4de99228
Seohyeon <0205lucy@naver.com>
2022-07-06 17:24:37 +0900

    :building_construction: 찜한메뉴 이미지 추가

--- src/heart/Heart.js
+++ src/heart/Heart.js
@@ -1,33 +1,20 @@
 import Header from "./Header";
 import Navbar from "../nav/Navbar";
 import styles from "./css/Heart.module.css";
-import products from "../result/products.json"
 
-function Heart({ name, tag, shopname, likenum, distance, price, img }) {
+function Heart() {
+    const path = "./img/";
   return (
     <div className={styles.background}>
         <div className={styles.container}>
             <Header></Header>
             <Navbar></Navbar>
 
-    {/* <div className={styles.section}>
-        <div className={styles.shopanddistance}>
-          <div className={styles.shopname}>[ {shopname} ]</div>
-          <div className={styles.distance}>
-            <img src={require(`./img/pin.png`)} alt="pin"></img>
-            <div className={styles.distnum}>{distance}</div>m
-          </div>
-        </div>
-        <div className={styles.name}>{name}</div>
-
-        <div className={styles.floor}>
-            <div className={styles.price}>{price}원</div>
-            <div className={styles.likenum}>
-                <img className={styles.ch} src={require(`./img/colored-heart.png`)} alt="noimg"></img>
-                {likenum}
+            <div className={styles.imgcontainer}>
+                <img src={require(`${path}heart1.png`)} className={styles.heartimg} alt="noimg"></img>
+                <img src={require(`${path}heart2.png`)} className={styles.heartimg} alt="noimg"></img>
+                <img src={require(`${path}heart3.png`)} className={styles.heartimg3} alt="noimg"></img>
             </div>
-        </div>
-      </div> */}
 
         </div>
     </div>
