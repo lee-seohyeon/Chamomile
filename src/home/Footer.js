5e4c522ab58e2e7dedde6f261aa23a7169fe497c
Seohyeon <0205lucy@naver.com>
2022-07-06 10:38:31 +0900

    :zap: 구글폼 링크 연결

--- src/home/Footer.js
+++ src/home/Footer.js
@@ -1,10 +1,13 @@
 import styles from "./css/Footer.module.css";
+import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.contactus}>Contact Us!</div>
       <div className={styles.firstfloor}>
-        <div className={styles.join}>내 카페 등록하기</div>
+        <div className={styles.join}>
+            <div onClick={() => window.open('https://forms.gle/AVm6kxDwUcvHRP8x8', '_blank')}>내 카페 등록하기</div>
+        </div>
       </div>
 
       <div className={styles.firstfloor}>
