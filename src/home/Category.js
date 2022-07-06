90866e3789f254c2b2a34db1481787027b608e0f
d0422 <rlfehd2013@naver.com>
2022-07-06 21:02:03 +0900

    :sparkles: 백엔드 연결!
    백엔드 연결

--- src/home/Category.js
+++ src/home/Category.js
@@ -4,46 +4,47 @@ function Category() {
   const path = "./img/";
   return (
     <div className={styles.category}>
-      <div className={styles.option}>
+      <Link to="/result/빵" className={styles.option}>
         <img
           src={require(`${path}bread.png`)}
           className={styles.img}
           alt="빵"
         ></img>
         빵
-      </div>
-      <div className={styles.option}>
+      </Link>
+
+      <Link to="/result/빙수" className={styles.option}>
         <img
           src={require(`${path}shaved ice.png`)}
           className={styles.img}
           alt="빙수"
         />
         빙수
-      </div>
-      <div className={styles.option}>
+      </Link>
+      <Link to="/result/마카롱" className={styles.option}>
         <img
           src={require(`${path}macaron.png`)}
           className={styles.img}
           alt="마카롱"
         />
         마카롱
-      </div>
-      <div className={styles.option}>
+      </Link>
+      <Link to="/result/케이크" className={styles.option}>
         <img
           src={require(`${path}birthday-cake.png`)}
           className={styles.img}
           alt="케이크"
         />
         케이크
-      </div>
-      <div className={styles.option}>
+      </Link>
+      <Link to="/result/아이스크림" className={styles.option}>
         <img
           src={require(`${path}icecream.png`)}
           className={styles.img}
           alt="아이스크림"
         />
         아이스크림
-      </div>
+      </Link>
       <Link to="/result/쿠키" className={styles.option}>
         <img
           src={require(`${path}cookie.png`)}
@@ -53,54 +54,55 @@ function Category() {
         쿠키
       </Link>
 
-      <div className={styles.option}>
+      <Link to="/result/스콘" className={styles.option}>
         <img
           src={require(`${path}scone.png`)}
           className={styles.imgscone}
           alt="스콘"
         />
         스콘
-      </div>
-      <div className={styles.option}>
+      </Link>
+      <Link to="/result/당고" className={styles.option}>
         <img
           src={require(`${path}dango.png`)}
           className={styles.img}
           alt="스콘"
         />
         당고
-      </div>
-      <div className={styles.option}>
+      </Link>
+      <Link to="/result/떡" className={styles.option}>
         <img
           src={require(`${path}ricecake.png`)}
           className={styles.imgrice}
           alt="스콘"
         />
         떡
-      </div>
-      <div className={styles.option}>
+      </Link>
+      <Link to="/result/파르페" className={styles.option}>
         <img
           src={require(`${path}parfait.png`)}
           className={styles.imgpar}
           alt="스콘"
         />
         파르페
-      </div>
-      <div className={styles.option}>
+      </Link>
+
+      <Link to="/result/타르트" className={styles.option}>
         <img
           src={require(`${path}tart.png`)}
           className={styles.imgegg}
           alt="스콘"
         />
         에그타르트
-      </div>
-      <div className={styles.option}>
+      </Link>
+      <Link to="/result/와플" className={styles.option}>
         <img
           src={require(`${path}waffle.png`)}
           className={styles.waffle}
           alt="스콘"
         />
         와플/크로플
-      </div>
+      </Link>
     </div>
   );
 }
