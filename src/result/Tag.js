1509f54ea76a033e7b79c22d1c6b87b2c6f9f674
d0422 <rlfehd2013@naver.com>
2022-07-08 15:14:17 +0900

    :zap: 백데이터 추가 및 태그 색상 변경

--- src/result/Tag.js
+++ src/result/Tag.js
@@ -1,6 +1,5 @@
 import styles from "./css/Tag.module.css";
 function Tag({ tag }) {
-  
   let tagcolor;
   if (tag === "꾸덕") {
     tagcolor = "#A37B4B";
@@ -20,6 +19,32 @@ function Tag({ tag }) {
     tagcolor = "#643D13";
   } else if (tag === "부드러움") {
     tagcolor = "#F2E056";
+  } else if (tag === "겉바속촉") {
+    tagcolor = "#E46C0A";
+  } else if (tag === "가성비") {
+    tagcolor = "#31859C";
+  } else if (tag === "겉바속촉") {
+    tagcolor = "#E46C0A";
+  } else if (tag === "안달아요") {
+    tagcolor = "#FF7E79";
+  } else if (tag === "토핑맛집") {
+    tagcolor = "#8052A2";
+  } else if (tag === "탱글") {
+    tagcolor = "#F79646";
+  } else if (tag === "바삭") {
+    tagcolor = "#D99694";
+  } else if (tag === "견과류") {
+    tagcolor = "#6A3B04";
+  } else if (tag === "수제") {
+    tagcolor = "#FAC090";
+  } else if (tag === "비쥬얼") {
+    tagcolor = "#236F9E";
+  } else if (tag === "고급짐") {
+    tagcolor = "#A6F9D4";
+  } else if (tag === "말랑") {
+    tagcolor = "#F2C0C1";
+  } else if (tag === "살살녹음") {
+    tagcolor = "#A6A6A6";
   } else {
     tagcolor = "#A7A7A7";
   }
