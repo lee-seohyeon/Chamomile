b16d4aef35f59c8ad9ad2b7159fef7fd8fadeb5c
Seohyeon <0205lucy@naver.com>
2022-08-17 16:16:28 +0900

    :lipstick: mypage 수정중

---
+++ src/mypage/Mypageprof.js
@@ -0,0 +1,55 @@
+import React from "react";
+import styles from "./css/Mypageprof.module.css";
+
+const Mypageprof = () => {
+	const path = "./img/";
+	return (
+		<>
+			<div className={styles.loginimg}>
+				<img
+					src={require(`${path}prof.png`)}
+					className={styles.img}
+					alt="noimg"
+				></img>
+				<div className={styles.logintext}>
+					<div className={styles.name}>김마일</div>
+					<div className={styles.email}>camomile@naver.com</div>
+				</div>
+			</div>
+
+			<hr />
+
+			<div className={styles.bar}>
+				<div className={styles.bardom}>
+					<img
+						src={require(`${path}recent.png`)}
+						className={styles.img}
+						alt="noimg"
+					></img>
+					<div className={styles.barname}>최근 본 상품</div>
+				</div>
+
+				<div className={styles.bardom}>
+					<img
+						src={require(`${path}review.png`)}
+						className={styles.img}
+						alt="noimg"
+					></img>
+					<div className={styles.barname}>MY 리뷰</div>
+				</div>
+				<div className={styles.bardom}>
+					<img
+						src={require(`${path}scrap.png`)}
+						className={styles.img}
+						alt="noimg"
+					></img>
+					<div className={styles.barname}>스크랩</div>
+				</div>
+			</div>
+
+            <hr/>
+		</>
+	);
+};
+
+export default Mypageprof;
